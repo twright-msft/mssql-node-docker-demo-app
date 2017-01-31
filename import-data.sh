ea8eb1b6d2e4ebc5cae30635f0bfe4100298af09
Travis Wright <twright@microsoft.com>
2017-01-30 23:42:08 -0800

    Adding even more time to the wait time

--- import-data.sh
+++ import-data.sh
@@ -1,5 +1,5 @@
 #wait for the SQL Server to come up
-sleep 60s
+sleep 90s
 
 #run the setup script to create the DB and the schema in the DB
 sqlcmd -S localhost -U sa -P Yukon900 -d master -i setup.sql

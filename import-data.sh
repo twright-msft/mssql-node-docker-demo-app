c43effa322b3826c5d9c46a8975cef2d6a76ef2c
Mike Casas <mkcsas0@gmail.com>
2017-07-20 08:07:28 -0400

    Simple typo.

--- import-data.sh
+++ import-data.sh
@@ -4,5 +4,5 @@ sleep 90s
 #run the setup script to create the DB and the schema in the DB
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Yukon900 -d master -i setup.sql
 
-#import the data from teh csv file
+#import the data from the csv file
 /opt/mssql-tools/bin/bcp DemoData.dbo.Products in "/usr/src/app/Products.csv" -c -t',' -S localhost -U sa -P Yukon900

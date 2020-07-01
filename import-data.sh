29c3a06c2d16e672b8a964a9729648ad630680f9
Travis Wright <twright@microsoft.com>
2020-06-30 23:24:46 -0700

    changing the import script to loop instead of wait

--- import-data.sh
+++ import-data.sh
@@ -1,8 +1,17 @@
-#wait for the SQL Server to come up
-sleep 20s
-
 #run the setup script to create the DB and the schema in the DB
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Yukon900 -d master -i setup.sql
+#do this in a loop because the timing for when the SQL instance is ready is indeterminate
+for i in {1..50};
+do
+    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Yukon900 -d master -i setup.sql
+    if [ $? -eq 0 ]
+    then
+        echo "setup.sql completed"
+        break
+    else
+        echo "not ready yet..."
+        sleep 1
+    fi
+done
 
 #import the data from the csv file
 /opt/mssql-tools/bin/bcp DemoData.dbo.Products in "/usr/src/app/Products.csv" -c -t',' -S localhost -U sa -P Yukon900
